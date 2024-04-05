7963178b9ba2ce79d4fdbdc7400018812335c7ae
atharvamulgund <mulgundatharva@gmail.com>
2024-04-05 23:50:24 +0530

    removed flowbite link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,7 @@ const Navbar = () => {
 
 <nav className="w-full">
     <div className="flex flex-wrap mx-4 justify-between sm:justify-around items-center sm:mx-auto max-w-screen-xl p-4">
-        <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Arthur</span>
-        </a>
         <div className="flex items-center space-x-6 rtl:space-x-reverse">
            <ModeToggle />
         </div>
